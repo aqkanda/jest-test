28da430117a2132fc9f76f29f528ae3c3d615c18
aqkanda <yuki.kanda@asia-quest.jp>
2022-05-06 17:57:37 +0900

    fix foreach 原因　forの中でreturnしていた

--- src/native/mock/foreach.ts
+++ src/native/mock/foreach.ts
@@ -1,5 +1,5 @@
 export function forEach(items: number[], callback: (item: number) => number) {
   for (let index = 0; index < items.length; index++) {
-    return callback(items[index]);
+    callback(items[index]);
   }
 }
